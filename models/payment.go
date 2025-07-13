bc1511da61904cf2598809207ed87ed376284b24
gustavoo151 <josegustavooliveira121@gmail.com>
2025-07-12 21:25:39 -0300

    Feat: PaymentRequest Struct

--- models/payment.go
+++ models/payment.go
@@ -1,8 +1,19 @@
 package models
 
+import (
+	_ "time"
+
+	_ "github.com/google/uuid"
+)
+
 type Payment struct {
 	CorrelationID string  `json:"correlationID"`
 	Amount        float64 `json:"amount"`
 	RequestedAt   string  `json:"requestedAt"`
 	ProcessedBy   string  `json:"processedBy"`
+}
+
+type PaymentRequest struct {
+	CorrelationID string  `json:"correlationID" binding:"required,uuid"`
+	Amount        float64 `json:"amount" binding:"required,gt=0"`
 }
