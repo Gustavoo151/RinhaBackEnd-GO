26cf3416aeaab2da741a57c8138fd71f910f0a86
gustavoo151 <josegustavooliveira121@gmail.com>
2025-07-13 05:04:01 -0300

    feat: New function to SetupRoutes wihout handlePayment functiom

--- api/router.go
+++ api/router.go
@@ -3,6 +3,7 @@ package api
 import (
 	"RinhaBackend/processor"
 	"RinhaBackend/storage"
+	"github.com/gin-gonic/gin"
 )
 
 type Router struct {
@@ -15,4 +16,18 @@ func NewRouter(strategy *processor.Strategy, repo *storage.Repository) *Router {
 		strategy: strategy,
 		repo:     repo,
 	}
+}
+
+func (r *Router) SetupRoutes() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+
+	// Middlewares
+	router.Use(gin.Recovery())
+
+	// Configurando as rotas
+	router.POST("/payments", r.handlePayment)
+	router.GET("/payments-summary", r.handleSummary)
+
+	return router
 }
