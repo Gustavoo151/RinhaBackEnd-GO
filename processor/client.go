a75ac1769ee5197743ae05bab0898f8bd9ea806d
gustavoo151 <josegustavooliveira121@gmail.com>
2025-07-12 21:37:35 -0300

    feat: Func to creat new Client

--- processor/client.go
+++ processor/client.go
@@ -1,9 +1,20 @@
 package processor
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type Client struct {
 	baseURL    string
 	name       string
 	httpClient *http.Client
+}
+
+func NewClient(baseURL, name string, timeout time.Duration) *Client {
+	return &Client{
+		baseURL:    baseURL,
+		name:       name,
+		httpClient: &http.Client{Timeout: timeout},
+	}
 }
