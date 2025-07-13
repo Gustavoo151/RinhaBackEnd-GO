df255a7be93c5d276e956bd7555ce558984f4590
gustavoo151 <josegustavooliveira121@gmail.com>
2025-07-13 04:45:26 -0300

    feat: func load Config

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -13,6 +14,26 @@ type Config struct {
 	HTTPTimeout          time.Duration
 	MaxConcurrent        int
 	DatabaseDSN          string
+}
+
+func load() *Config {
+	port := getEnv("PORT", "9999")
+	defaultURL := getEnv("PAYMENT_PROCESSOR_URL_DEFAULT", "http://payment-processor-default:8080")
+	fallbackURL := getEnv("PAYMENT_PROCESSOR_URL_FALLBACK", "http://payment-processor-fallback:8080")
+
+	healthCheckInterval, _ := strconv.Atoi(getEnv("HEALTH_CHECK_INTERVAL", "5"))
+	httpTimeout, _ := strconv.Atoi(getEnv("HTTP_TIMEOUT", "2"))
+	maxConcurrent, _ := strconv.Atoi(getEnv("MAX_CONCURRENT", "1000"))
+
+	return &Config{
+		Port:                 port,
+		DefaultProcessorURL:  defaultURL,
+		FallbackProcessorURL: fallbackURL,
+		HealthCheckInterval:  time.Duration(healthCheckInterval) * time.Second,
+		HTTPTimeout:          time.Duration(httpTimeout) * time.Second,
+		MaxConcurrent:        maxConcurrent,
+		DatabaseDSN:          getEnv("DATABASE_DSN", "postgres://postgres:postgres@db:5432/rinha?sslmode=disable"),
+	}
 }
 
 func getEnv(key, defaultValue string) string {
